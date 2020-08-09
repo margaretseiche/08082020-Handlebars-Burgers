3b47330c9ab502f267ad5a12c4270646f1d01379
Margaret Seiche <margaretseiche@hotmail.com>
2020-08-09 00:10:20 -0400

    revision

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,13 @@
 const mysql = require("mysql");
+require("dotenv").config();
+console.log(process.env.PRODUCTION);
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    PORT: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
+const connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection(process.env.PRODUCTION);
+};    
 
 connection.connect(function(err) { 
     if (err) {
