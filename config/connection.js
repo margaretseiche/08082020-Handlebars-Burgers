761290cf07508cb7b767ca41bad462f79c476415
Margaret Seiche <margaretseiche@hotmail.com>
2020-08-25 10:16:13 -0400

    fixed connection problem

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 const mysql = require("mysql");
-require("dotenv").config();
-console.log(process.env.PRODUCTION); 
+// require("dotenv").config();
+// console.log(process.env.PRODUCTION); 
 
-const connection;
+var connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection(process.env.PRODUCTION);
+    // connection = mysql.createConnection(process.env.PRODUCTION);
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        PORT: 3306,
+        database: "burgers_db"
+    });
 };    
 
 connection.connect(function(err) { 
